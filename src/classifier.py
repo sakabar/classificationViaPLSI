4cd5bd3df178611a8a634ed316f91ffac57b93c3
sakabar <takafumi.sakakibara@gmail.com>
2014-06-18 23:59:37 +0900

    Optimize: remove if hoge in lst

--- src/classifier.py
+++ src/classifier.py
@@ -6,12 +6,18 @@ import collections
 class Classifier:
   def __init__(self, k, n_v_list):
     self.k = k
-    self.n_v_list = n_v_list
 
-    self.noun_list = list(set(map(lambda (n,v): n, n_v_list)))
-    self.verb_list = list(set(map(lambda (n,v): v, n_v_list)))
+    #self.n_v_listなどは重復なし
+    self.n_v_list = list(set(n_v_list))
+    self.noun_list = (list(set(map(lambda (n,v): n, n_v_list))))
+    self.verb_list = (list(set(map(lambda (n,v): v, n_v_list))))
+
+    #個数はv_v_counterで記録
     self.n_v_counter = collections.Counter(n_v_list)
 
+    # print "noun_list: " + str(len(self.noun_list)) #debug
+    # print "verb_list: " + str(len(self.verb_list)) #debug
+    
     #初期化
     self.init_log_p_z()
     self.init_log_p_x_z()
@@ -72,7 +78,7 @@ class Classifier:
 
   def learn_Mstep(self):
     #P(z)を更新
-    print "M-step: P(z)" #debug
+    # print "M-step: P(z)" #debug
     deno = 0
     for j in xrange(0, self.k):
       for (noun, verb) in self.n_v_list:
@@ -80,7 +86,7 @@ class Classifier:
     log_deno = math.log10(deno)
 
     for i in xrange(0, self.k):
-      print ("z_" + str(i)) #debug
+      # print ("z_" + str(i)) #debug
       nume = 0
       for (noun, verb) in self.n_v_list:
         nume += self.n_v_counter[(noun, verb)] * (10 ** self.log_p_z_xy[noun][verb][i])
@@ -90,28 +96,31 @@ class Classifier:
 
 
     #P(x | z)を更新
-    print "M-step: P(x | z)" #debug
+    # print "M-step: P(x | z)" #debug
     for i in xrange(0, self.k):
-      print ("z_" + str(i)) #debug
+      # print ("z_" + str(i)) #debug
       deno = 0
       for (x, y) in self.n_v_list:
         deno += self.n_v_counter[(x, y)] * (10 ** self.log_p_z_xy[x][y][i])
       log_deno = math.log10(deno)
 
       for noun in self.noun_list:
+        # print ("  " + noun) #debug
         nume = 0
         for verb in self.verb_list:
-          if (noun, verb) in self.n_v_list:
-            nume += self.n_v_counter[(noun, verb)] * (10 ** self.log_p_z_xy[noun][verb][i])
+          # print ("    " + verb) #debug
+
+          # これがボトルネックだった
+          # if (noun, verb) in self.n_v_list:
+          nume += self.n_v_counter[(noun, verb)] * (10 ** self.log_p_z_xy[noun][verb][i])
         log_nume = math.log10(nume)
           
-        self.log_p_x_z = log_nume - log_deno
-
+        self.log_p_x_z[noun][i] = log_nume - log_deno
 
     #P(y | z)を更新
-    print "M-step: P(y | z)" #debug
+    # print "M-step: P(y | z)" #debug
     for i in xrange(0, self.k):
-      print ("z_" + str(i)) #debug
+      # print ("z_" + str(i)) #debug
       deno = 0
       for (x, y) in self.n_v_list:
         deno += self.n_v_counter[(x, y)] * (10 ** self.log_p_z_xy[x][y][i])
@@ -120,17 +129,17 @@ class Classifier:
       for verb in self.verb_list:
         nume = 0
         for noun in self.noun_list:
-          if (noun, verb) in self.n_v_list:
-            nume += self.n_v_counter[(noun, verb)] * (10 ** self.log_p_z_xy[noun][verb][i])
+          nume += self.n_v_counter[(noun, verb)] * (10 ** self.log_p_z_xy[noun][verb][i])
         log_nume = math.log10(nume)
           
-        self.log_p_x_z = log_nume - log_deno
+        self.log_p_y_z[verb][i] = log_nume - log_deno
 
     return
 
   def learnEM(self):
     self.learn_Estep()
     self.learn_Mstep()
+
     return
 
 
@@ -141,8 +150,8 @@ class Classifier:
   def get_n_v_list(self):
     return self.n_v_list
 
-  def get_log_p_x_z(self):
-    return self.log_p_x_z
+  # def get_log_p_x_z(self):
+  #   return self.log_p_x_z
 
   def get_log_p_y_z(self):
     return self.log_p_y_z
@@ -150,9 +159,10 @@ class Classifier:
   def get_log_p_z_xy(self):
     return self.log_p_z_xy
 
-  def get_log_p_x_y(self, n, v):
+  def get_log_p_x_y(self, noun, verb):
     ans = 0.0
     for i in xrange(0, self.k):
-      ans += 10**(self.log_p_z[i] + self.log_p_x_z[n][i] + self.log_p_y_z[v][i])
+      # ans += 10.0 ** (self.log_p_z[i] + self.log_p_x_z[noun][i] + self.log_p_y_z[verb][i])
+      ans += 10.0 ** (self.log_p_x_z[noun][i])
     return math.log10(ans)
 
